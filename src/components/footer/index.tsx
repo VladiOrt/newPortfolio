4634b902bb39bc094def4fee7ea0509149e85eef
Vladi Ortega <vladi_ortega@hotmail.com>
2023-04-10 19:09:40 -0600

    update

---
+++ src/components/footer/index.tsx
@@ -0,0 +1,19 @@
+import './index.scss'
+
+const Footer = () =>{
+    return(
+        <div className="containerFooter">
+            <section className='leftContainerFooter'>
+
+            </section>
+            <section className='centerContainerFooter'>
+                
+            </section>
+            <section className='rightContainerFooter'>
+                
+            </section>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,24 @@
+import './index.scss'
+
+const NavBar = () =>{
+    return(
+        <div className="containerNavBar">
+            <section className='logoContainer'>
+
+            </section>
+            <section className='MenuContainer'>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+            </section>
+            <section className='spaceRight'>
+
+            </section>
+        </div>
+    )
+}
+
+export default NavBar;
